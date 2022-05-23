c72f10bbe635a8f55d11b21816d406eeb7633402
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-05-23 11:51:33 +0600

    tools added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,9 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+app.use(cors());
+app.use(express.json());
+
 async function run() {
   try {
     await client.connect();
@@ -18,12 +22,21 @@ async function run() {
     const projectsCollection = client
       .db("partsManufacturer")
       .collection("projects");
+    const productsCollection = client
+      .db("partsManufacturer")
+      .collection("products");
 
     app.get("/projects", async (req, res) => {
       const query = {};
       const cursor = await projectsCollection.find(query);
       const projects = await cursor.toArray();
       res.send(projects);
+    });
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = await productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
     });
   } finally {
   }
